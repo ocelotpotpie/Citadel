f47093ac503068af1ba2eaf43eb84a09818380da
JonnyD <contact@jonnydevine.com>
2013-05-17 22:55:51 +0100

    Refactored Dao

--- src/com/untamedears/citadel/dao/CitadelDao.java
+++ src/com/untamedears/citadel/dao/CitadelDao.java
@@ -2,7 +2,6 @@ package com.untamedears.citadel.dao;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -23,24 +22,11 @@ import com.avaje.ebean.config.ServerConfig;
 import com.lennardf1989.bukkitex.MyDatabase;
 import com.untamedears.citadel.Citadel;
 import com.untamedears.citadel.DbUpdateAction;
-import com.untamedears.citadel.entity.Faction;
-import com.untamedears.citadel.entity.FactionMember;
-import com.untamedears.citadel.entity.Member;
-import com.untamedears.citadel.entity.Moderator;
-import com.untamedears.citadel.entity.PersonalGroup;
 import com.untamedears.citadel.entity.IReinforcement;
 import com.untamedears.citadel.entity.PlayerReinforcement;
 import com.untamedears.citadel.entity.ReinforcementKey;
+import com.untamedears.citadel.entity.Test;
 
-/**
- * Created by IntelliJ IDEA.
- * User: chrisrico
- * Date: 3/18/12
- * Time: 3:58 PM
- * 
- * Last modified by JonnyD
- * 7/18/12
- */
 public class CitadelDao extends MyDatabase {
 	private static final int CHUNK_SIZE = 16;
 
@@ -75,9 +61,9 @@ public class CitadelDao extends MyDatabase {
     @Override
     protected List<Class<?>> getDatabaseClasses() {
         return Arrays.asList(
-                Faction.class, Member.class, FactionMember.class,
-                PlayerReinforcement.class, ReinforcementKey.class,
-                PersonalGroup.class, Moderator.class);
+                PlayerReinforcement.class, 
+                ReinforcementKey.class,
+                Test.class);
     }
 
     public Object save(Object object) {
@@ -87,75 +73,6 @@ public class CitadelDao extends MyDatabase {
 
     public void delete(Object object) {
         getDatabase().delete(object);
-    }
-
-    public Faction getOrCreateFaction(String name, String founder) {
-        Faction faction = findGroupByName(name);
-        if (faction == null) {
-            faction = new Faction(name, founder);
-            save(faction);
-        }
-        return faction;
-    }
-    
-    public Set<Faction> findGroupsByFounder(String founder){
-    	return getDatabase().createQuery(Faction.class, "find faction where founder = :founder")
-    			.setParameter("founder", founder)
-    			.findSet();
-    }
-    
-    public Set<FactionMember> findGroupsByMember(String memberName){
-    	return getDatabase().createQuery(FactionMember.class, "find factionMember where memberName = :memberName")
-    			.setParameter("memberName", memberName)
-    			.findSet();
-    }
-    
-    public Set<Moderator> findGroupsByModerator(String memberName){
-    	return getDatabase().createQuery(Moderator.class, "find moderator where memberName = :memberName")
-    			.setParameter("memberName", memberName)
-    			.findSet();
-    }
-
-	public Set<Faction> findAllGroups() {
-		return getDatabase().createQuery(Faction.class, "find faction")
-				.findSet();
-	}
-	
-	public Set<FactionMember> findAllGroupMembers(){
-		return getDatabase().createQuery(FactionMember.class, "find factionMember")
-				.findSet();
-	}
-	
-	public Faction findGroup(String groupName){
-		return getDatabase().createQuery(Faction.class, "find faction where name = :groupName")
-				.setParameter("groupName", groupName)
-				.findUnique();
-	}
-
-    public Faction findGroupByName(String name) {
-        return getDatabase().createQuery(Faction.class, "find faction where name = :name")
-                .setParameter("name", name)
-                .findUnique();
-    }
-    
-    public boolean hasGroupMember(String groupName, String memberName) {
-        return getDatabase().createQuery(FactionMember.class, "find factionMember where faction_name = :groupName" +
-        		" and member_name = :memberName")
-        		.setParameter("groupName", groupName)
-                .setParameter("memberName", memberName)
-                .findRowCount() > 0;
-    }
-
-    public FactionMember findGroupMember(String groupName, String memberName) {
-        return getDatabase().createQuery(FactionMember.class, "find factionMember where memberName = :memberName")
-                .setParameter("memberName", memberName)
-                .findUnique();
-    }
-    
-    public Set<FactionMember> findMembersOfGroup(String groupName) {
-        return getDatabase().createQuery(FactionMember.class, "find factionMember where factionName = :groupName")
-        		.setParameter("groupName", groupName)
-                .findSet();
     }
     
     public Set<IReinforcement> findReinforcementsByGroup(String groupName){
@@ -208,14 +125,6 @@ public class CitadelDao extends MyDatabase {
         }
         return new TreeSet<IReinforcement>(result);
     }
-
-    public void moveReinforcements(String from, String target){
-    	SqlUpdate update = getDatabase().createSqlUpdate("UPDATE reinforcement SET name = :target, security_level = 1" +
-    			" WHERE name = :from")
-    			.setParameter("target", target)
-    			.setParameter("from", from);
-    	getDatabase().execute(update);
-    }
     
     public int countReinforcements(){
     	SqlRow row = getDatabase().createSqlQuery("select count(*) as count from reinforcement").findUnique();
@@ -232,60 +141,6 @@ public class CitadelDao extends MyDatabase {
     			.setParameter("founder", playerName)
     			.findUnique();
     	return row.getInteger("count"); 
-	}
-
-	public Set<Member> findAllMembers() {
-		return getDatabase().createQuery(Member.class, "find member")
-    			.findSet();
-	}
-	
-	public Member findMember(String memberName){
-		return getDatabase().createQuery(Member.class, "find member where member_name = :memberName")
-				.setParameter("memberName", memberName)
-				.findUnique();
-	}
-
-	public Set<PersonalGroup> findAllPersonalGroups() {
-		return getDatabase().createQuery(PersonalGroup.class, "find personalGroup")
-				.findSet();
-	}
-
-	public void addGroup(String groupName) {
-		SqlUpdate update = getDatabase().createSqlUpdate("INSERT INTO faction (name, founder) VALUES (:groupName, 'Gu3rr1lla')")
-				.setParameter("groupName", groupName);
-		getDatabase().execute(update);		
-	}
-
-	public PersonalGroup findPersonalGroup(String ownerName) {
-		return getDatabase().createQuery(PersonalGroup.class, "find personalGroup where owner_name = :ownerName")
-				.setParameter("ownerName", ownerName)
-				.findUnique();
-	}
-	
-    public boolean hasGroupModerator(String groupName, String memberName) {
-        return getDatabase().createQuery(Moderator.class, "find moderator where faction_name = :groupName" +
-        		" and member_name = :memberName")
-        		.setParameter("groupName", groupName)
-                .setParameter("memberName", memberName)
-                .findRowCount() > 0;
-    }
-
-	public Set<Moderator> findModeratorsOfGroup(String groupName) {
-		return getDatabase().createQuery(Moderator.class, "find moderator where faction_name = :groupName")
-				.setParameter("groupName", groupName)
-				.findSet();
-	}
-	
-	public void removeAllMembersFromGroup(String groupName){
-		SqlUpdate update = getDatabase().createSqlUpdate("delete from faction_member where faction_name = :groupName")
-				.setParameter("groupName", groupName);
-		getDatabase().execute(update);
-	}
-	
-	public void removeAllModeratorsFromGroup(String groupName){
-		SqlUpdate update = getDatabase().createSqlUpdate("delete from moderator where faction_name = :groupName")
-				.setParameter("groupName", groupName);
-		getDatabase().execute(update);
 	}
 	
 	public void executeUpdateQuery(String query) {
