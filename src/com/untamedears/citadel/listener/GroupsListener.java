e678c2c1ca52fa6e39c79f3fef592d1304b8fb7e
JonnyD <contact@jonnydevine.com>
2013-05-18 17:40:48 +0100

    Fixed imports

--- src/com/untamedears/citadel/listener/GroupsListener.java
+++ src/com/untamedears/citadel/listener/GroupsListener.java
@@ -1,19 +1,12 @@
 package com.untamedears.citadel.listener;
 
-import java.util.List;
-import java.util.Set;
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.plugin.Plugin;
 
-import com.avaje.ebean.SqlRow;
-import com.avaje.ebean.SqlUpdate;
 import com.untamedears.citadel.Citadel;
 import com.untamedears.citadel.dao.CitadelDao;
-import com.untamedears.citadel.entity.Faction;
-import com.untamedears.citadel.manager.GroupManager;
 
 public class GroupsListener implements Listener {
 
